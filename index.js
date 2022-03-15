00722eeee9f2b352629cf2d5fc3fbf4405bcc129
DLFDK <dennislythfrederiksen@gmail.com>
2022-03-15 02:44:13 +0100

    Add inline worker to index.js

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ fetch("https://tile.openstreetmap.org/0/0/0.png").catch(error => {
     console.log("Index.js failed to fetch");
 });
 
-const worker = new Worker("worker.js");
+const externalWorker = new Worker("worker.js");
+
+const inlineWorker = new Worker(URL.createObjectURL(new Blob([`(${workerFunction.toString()})()`], { type: 'text/javascript' })));
+
+function workerFunction() {
+    fetch("https://tile.openstreetmap.org/0/0/0.png").catch(error => {
+        console.log("External Worker failed to fetch");
+    });
+}
--- worker.js
+++ worker.js
@@ -1,3 +1,3 @@
 fetch("https://tile.openstreetmap.org/0/0/0.png").catch(error => {
-    console.log("Worker failed to fetch");
+    console.log("External Worker failed to fetch");
 });
